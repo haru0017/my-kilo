0f89c646b4967d643f71f11337d813db22fbe1fd
Tanaka Haruto <hammerhead0017@gmail.com>
2021-02-15 23:22:15 +0900

    Turn off echoing

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
+
     char buf;
     while (read(STDIN_FILENO, &buf, 1) == 1 && buf != 'q');
     return 0;
