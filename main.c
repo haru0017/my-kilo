3547fb4ec714e2e416a2a3fc71c05c2c5531ffde
Tanaka Haruto <hammerhead0017@gmail.com>
2021-02-26 22:21:08 +0900

    Clear the screen

--- main.c
+++ main.c
@@ -50,6 +50,12 @@ char editorReadKey() {
     return c;
 }
 
+/*** output ***/
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -69,6 +75,7 @@ int main() {
     enableRawMode();
 
     while (1) {
+        editorRefreshScreen();
         editorProcessKeypress();
     }
 
