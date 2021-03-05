528543313129ead5b01803780bd7367159fc511b
Tanaka Haruto <hammerhead0017@gmail.com>
2021-03-05 20:18:14 +0900

    Welcome message

--- main.c
+++ main.c
@@ -121,10 +121,21 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
+#define KILO_VERSION "0.0.1"
+
 void editorDrawRows(struct abuf *ab) {
-    for (int y = 0; y < E.screenrows; y++)
-    {
-        abAppend(ab, "~", 1);
+    for (int y = 0; y < E.screenrows; y++) {
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "my-kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding--) abAppend(ab, "~", 1);
+            while (padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
 
         abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
